d369c3e9d560020026b36e7b588bb5163a5af2d3
Esteban Rebuffé-Mareau <estebanianis@gmail.com>
2024-10-29 21:59:17 +0100

    qualification css to tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
 import NavBar from "./components/NavBar";
 import ProjectsSection from "./components/ProjectSection";
-import Qualifications from "./components/Qualifications/Qualifications";
+import Qualifications from "./components/Qualifications";
 
 export default function Home() {
   return (
